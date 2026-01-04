7c5f27597a994fd180fd584dbe0427c81f06d800
Digvijaykarande <karandedigvijay2@gmail.com>
2026-01-04 14:59:08 +0530

    updated portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/",   // ✅ MUST be here (top-level)
   server: {
-    host: "::",
     port: 8080,
   },
   plugins: [
